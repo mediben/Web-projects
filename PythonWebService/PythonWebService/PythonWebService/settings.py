71ae40ed0c043d5cda347779a40fe4e12ccf30b5
DATAPROCES75\DATAPROCES <mbt@dataproces.dk>
2018-02-23 11:15:39 +0100

    Add model Update DB driver to MySQL

--- PythonWebService/PythonWebService/PythonWebService/settings.py
+++ PythonWebService/PythonWebService/PythonWebService/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'restapi.apps.RestapiConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'PythonWebService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'database_api',               
+        'USER': 'root',                       
+        'PASSWORD': '',                      
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- PythonWebService/PythonWebService/restapi/models.py
+++ PythonWebService/PythonWebService/restapi/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = (
+    ('active', 'ACTIVE'),
+    ('pending', 'PENDING'),
+    ('inactive', 'INACTIVE'),
+    ('deleted', 'DELETED'),
+)
+
+
+TITLE_CHOICES = (
+    ('executive', 'EXECUTIVE'),
+    ('manager', 'MANAGER'),
+    ('employees', 'EMPLOYEES'),
+)
+
+
+class Employee(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(choices=TITLE_CHOICES, default='EMPLOYEES', max_length=100)
+    fullname = models.CharField(max_length=255, default='')
+    status = models.CharField(choices=STATUS_CHOICES, default='ACTIVE', max_length=100)
+    salary = models.FloatField(max_length=25, default=0)
+    alerts = models.IntegerField( default=0)
+    address = models.TextField()
+
+    class Meta:
+        ordering = ('created',)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=255, default='')
+    employees = models.ManyToManyField(Employee)
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=255, default='')
+    address = models.TextField()
+    departments = models.ManyToManyField(Department)
+
+    class Meta:
+        ordering = ('name',)
